7de48a659c356b1f9770d2b17eccf4f310445f33
RusSwed <vladrusshvedov@mail.ru>
2024-02-20 23:44:59 +0300

    Update
    Начал динамический массив структур
    Добавил функции для вычисления площадей и перимитра

--- main.cpp
+++ main.cpp
@@ -11,49 +11,63 @@ struct coord{
 struct figure{
     int count_side;
     double lenght_side;
-    double S;
-    double P;
-    double x;
-    double y;
+    double square;
+    double perimeter;
+    coord coord_side;
 };
 
-void input(figure *number,int i);
-void square(figure *number,int i);
+void input(figure *number,int size_array);
+void get_square(figure *number, int size_array);
+void get_perimeter(figure *number, int size_array);
 
 int main()
 {
     setlocale(LC_CTYPE,"");
-    int size1;
-    cin >> size1;
+    int size_array;
+    cin >> size_array;
 
-    figure number[size1];
+    figure* number = new figure[size_array];
 
     figure *p_number = number;
-    //cout << p_number << endl;
 
     input(p_number,0);
 
+    get_square(p_number, 0);
+
+    get_perimeter(p_number, 0);
+
     return 0;
 }
-void input(figure *number,int i){
+void input(figure *number,int size_array){
     cout <<"¬ведите количество сторон\n";
-    cin >>number[i].count_side;
+    cin >>number[size_array].count_side;
     cout <<"¬ведите длину стороны\n";
-    cin >>number[i].lenght_side;
-    cout <<"¬ведите координату x\n";
-    cin >>number[i].x;
-    cout <<"¬ведите координату y\n";
-    cin >>number[i].y;
+    cin >>number[size_array].lenght_side;
     return;
 }
-void square(figure *number,int i){
-    if(number[i].count_side==3){
-        number[i].S=(number[i].lenght_side*number[i].lenght_side)*sqrt(3)/2;
-    }
-    else if(number[i].count_side==4){
-        number[i].S=(number[i].lenght_side*number[i].lenght_side);
-    }
-    else{
 
+void get_square(figure *number, int size_array){
+    if(number[size_array].count_side == 3){
+        number[size_array].square = (pow(number[size_array].lenght_side,2)*sqrt(3))/4;
     }
+    else if(number[size_array].count_side == 4){
+        number[size_array].square = number[size_array].lenght_side*number[size_array].lenght_side;
+    }
+    else if(number[size_array].count_side > 4)
+    {
+        number[size_array].square = (number[size_array].count_side*pow(number[size_array].lenght_side, 2))/(4*tan((2*M_PI)/(2*number[size_array].count_side)));
+    }
+    else
+    {
+        number[size_array].square = 0;
+    }
+
+    cout << "Square = " << number[size_array].square << '\n';
 }
+
+void get_perimeter(figure *number, int size_array){
+    number[size_array].perimeter = number[size_array].count_side*number[size_array].lenght_side;
+
+    cout << "Perimeter = " << number[size_array].perimeter;
+}
+
