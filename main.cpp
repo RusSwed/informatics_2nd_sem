c1916189d88dc941c4061ebeb6b1f1e5abbf3d60
RusSwed <vladrusshvedov@mail.ru>
2024-02-26 00:05:19 +0300

    Add get_coord
    Первая версия функции, описывающая нахождение координат

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ struct figure{
 void input(figure *number,int size_array);
 void get_square(figure *number, int size_array);
 void get_perimeter(figure *number, int size_array);
+void get_coord(figure *number, int size_array);
 
 int main()
 {
@@ -36,6 +37,8 @@ int main()
 
     get_perimeter(p_number, 0);
 
+    get_coord(p_number, 0);
+
     return 0;
 }
 void input(figure *number,int size_array){
@@ -43,6 +46,10 @@ void input(figure *number,int size_array){
     cin >>number[size_array].count_side;
     cout <<"¬ведите длину стороны\n";
     cin >>number[size_array].lenght_side;
+    cout <<"¬ведите координату x\n";
+    cin >>number[size_array].coord_side.x;
+    cout <<"¬ведите координату y\n";
+    cin >>number[size_array].coord_side.y;
     return;
 }
 
@@ -71,3 +78,24 @@ void get_perimeter(figure *number, int size_array){
     cout << "Perimeter = " << number[size_array].perimeter;
 }
 
+void get_coord(figure *number, int size_array){
+    double x_centre;
+    double y_centre;
+    double R;
+    double x_coord, y_coord;
+    double alpha, angle_step, angle;
+
+    R = (number[size_array].lenght_side)/(2*sin(M_PI/number[size_array].lenght_side));
+    x_centre = number[size_array].coord_side.x - R*cos((2*M_PI)/(number[size_array].lenght_side));
+    y_centre = number[size_array].coord_side.y - R*sin((2*M_PI)/(number[size_array].lenght_side));
+    alpha = atan2(number[size_array].coord_side.x - x_centre, number[size_array].coord_side.y - y_centre);
+    angle_step = M_PI/number[size_array].lenght_side;
+
+    for(int i = 2; i <= number[size_array].count_side; i++){
+        angle = angle_step*i;
+        x_coord = x_centre + R*cos(angle);
+        y_coord = y_centre + R*sin(angle);
+        cout << "Coord X" << i << "=" << x_coord << '\n';
+        cout << "Coord Y" << i << "=" << y_coord << '\n';
+    }
+}
